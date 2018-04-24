fcd878958b2321d7bea0d7929b246c68933918dd
JorgeFrias <100317118@alumnos.uc3m.es>
2018-04-24 13:24:53 +0200

    Eliminados unos ficheros que no debían estar en la carpeta.

--- midiReader.py
+++ midiReader.py
@@ -1,6 +1,8 @@
 import mido
 import os
 import numpy as np
+from idna import alabel
+
 
 class note():
     def __init__(self, note : int, velocity : int, time : int):
@@ -21,16 +23,36 @@ def readTrack(mid : mido.MidiFile, printInfo = True):
                     notes.append(tmpNote)
 
                 except AttributeError:
-                    print('> No attribute channel, note or velocity')
+                    if printInfo:
+                        print('> No attribute channel, note or velocity')
 
     return notes
+
+def run():
+    allSongsNotes = []
+
+    ''' Fetch all '''
+    cwd = os.getcwd()  # Current working directory
+    midiDB = 'Midi'
+    composerDir = os.path.join(cwd, midiDB)
+    for composer in os.listdir(composerDir):                    # iterate over artists
+        compSongs = os.path.join(cwd, midiDB, composer)
+        for song in os.listdir(compSongs):                      # iterate over songs
+            songPath = os.path.join(cwd, midiDB, composer, song)
+            print('> Reading ' + composer + ': ' + song)
+            mid = mido.MidiFile(songPath)                       # MIDI info
+            allSongsNotes.extend(readTrack(mid, False))                # Append new notes
+
+    '''Buld datasets '''
+    notesInInput = 3                                            # To select how many previous notes to use
+    genertateDataSet(allSongsNotes, notesInInput)
 
 def genertateDataSet(notes, dataSize):
     '''
     Creates the nunmpy arrays for sklearn naive_bayes
     :param notes: The array with notes obj
     :param dataSize: Number of notes to build the training data set
-    :return:
+    :return: Dict dataNotes, targetNotes, targetVelocity, targetTime
     '''
     currentPos = 0                                              # Note to start getting dataSize notes. Next target size notes
     npDataNotes = np.array([])
@@ -39,11 +61,9 @@ def genertateDataSet(notes, dataSize):
     npTargetTime = np.array([])
 
     for i in range(len(notes) - (dataSize + 1)):                # Not to go over the end
-        # Get data positions
+        # Get data notes
         for i in range(dataSize):
-            # dataPositions.append(currentPos + i)
             tmpPos = currentPos + i
-            # dataNotes.append(notes[tmpPos])
             np.append(npDataNotes, [notes[tmpPos].note, notes[tmpPos].velocity, notes[tmpPos].time])
 
         # Get target note, vel and time
@@ -54,19 +74,6 @@ def genertateDataSet(notes, dataSize):
 
     return {'dataNotes':npDataNotes, 'targetNotes':npTargetNotes,'targetVelocity':npTargetVelocity, 'targetTime':npTargetTime}
 
-''' Fetch all '''
-cwd = os.getcwd()                                               # Current working directory
-midiDB = 'Midi'
-composerDir = os.path.join(cwd, midiDB)
-for composer in os.listdir(composerDir):                        # iterate over artists
-    compSongs = os.path.join(cwd, midiDB, composer)
-    for song in os.listdir(compSongs):                          # iterate over songs
-        songPath = os.path.join(cwd, midiDB, composer, song)
-        print(composer + ': ' + song)
-
-        mid = mido.MidiFile(songPath)                           # MIDI info
-        readTrack(mid)
 
 
-
-
+run()
