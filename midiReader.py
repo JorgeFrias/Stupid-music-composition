0437a55b90c2cc5ac250d073a8993cc423b35ccc
JorgeFrias <100317118@alumnos.uc3m.es>
2018-04-24 12:05:56 +0200

    Reading notes from file.

--- midiReader.py
+++ midiReader.py
@@ -1,5 +1,43 @@
 import mido
+import os
 
-mid = mido.MidiFile('song.mid')
-for msg in mid.play():
-    port.send(msg)
+
+class note():
+    def __init__(self, note : int, velocity : int, time : int):
+        self.velocity = velocity
+        self.note = note
+        self.time = time
+
+def readTrack(mid : mido.MidiFile, printInfo = True):
+    notes = []
+    for i, track in enumerate(mid.tracks):
+        if printInfo:
+            print('Track {}: {}'.format(i, track.name))
+        for msg in track:
+            if not msg.is_meta:
+                # Read content
+                try:
+                    tmpNote = note(msg.note, msg.velocity, msg.time)
+                    notes.append(tmpNote)
+
+                except AttributeError:
+                    print('> No attribute channel, note or velocity')
+
+    return notes
+
+''' Fetch all '''
+cwd = os.getcwd()                                               # Current working directory
+midiDB = 'Midi'
+composerDir = os.path.join(cwd, midiDB)
+for composer in os.listdir(composerDir):                        # iterate over artists
+    compSongs = os.path.join(cwd, midiDB, composer)
+    for song in os.listdir(compSongs):                          # iterate over songs
+        songPath = os.path.join(cwd, midiDB, composer, song)
+        print(composer + ': ' + song)
+
+        mid = mido.MidiFile(songPath)                           # MIDI info
+        readTrack(mid)
+
+
+
+
